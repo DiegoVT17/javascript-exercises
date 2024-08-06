ff44c1e028c424d80621f01917bf0ecc8bb01f23
Diego Vera <173311520+DiegoVT17@users.noreply.github.com>
2024-08-05 21:58:52 -0500

    add leapYears and tempConversion solutions

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // years divisible by 4
+  // years divisible by 100 not leap unless divisible by 400
 
-};
+  if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    return true
+  }
+
+  return false
+}
 
 // Do not edit below this line
-module.exports = leapYears;
+module.exports = leapYears
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,26 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function (degrees) {
+  const conversion = (degrees - 32) * (5 / 9)
 
-const convertToFahrenheit = function() {
-};
+  if (!Number.isInteger(conversion)) {
+    return Number(Number.parseFloat(conversion).toFixed(1))
+  }
+
+  return conversion
+}
+
+const convertToFahrenheit = function (degrees) {
+  // (32 °C × 9/5) + 32 = 89.6 °F
+  const conversion = degrees * (9 / 5) + 32
+
+  if (!Number.isInteger(conversion)) {
+    return Number(Number.parseFloat(conversion).toFixed(1))
+  }
+
+  return conversion
+}
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
-};
+  convertToFahrenheit,
+}
