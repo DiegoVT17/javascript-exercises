c5b01b5e80594ed5a318d5e7488d28a2a41daa59
Diego Vera <173311520+DiegoVT17@users.noreply.github.com>
2024-08-05 21:21:36 -0500

    add summAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  const sumArr = []
 
-};
+  if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR"
+  } else if (a > b) {
+    const temp = a // temp variable
+    a = b
+    b = temp
+  }
+
+  for (let i = a; i <= b; i++) {
+    sumArr.push(i)
+  }
+
+  return sumArr.reduce((acc, current) => acc + current, 0)
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
